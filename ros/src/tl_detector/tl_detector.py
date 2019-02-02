3a5fcc4170d642a4c81e79b0d57110cb577f0c7c
dinesh-i <dineshkumar.ilangovan@gmail.com>
2019-02-02 18:35:19 +0000

    Adds launch for churchlot

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -164,6 +164,7 @@ class TLDetector(object):
         # List of positions that correspond to the line to stop in front of for a given intersection
         stop_line_positions = self.config['stop_line_positions']
         if(self.pose):
+            rospy.loginfo("car_wp_idx is assigned")
             car_wp_idx = self.get_closest_waypoint(self.pose.pose.position.x, self.pose.pose.position.y)
 
         #TODO find the closest visible traffic light (if one exists)
